2feb8d146cade7bd7509977a55d258347912632e
muntakim1 <muntakim1104001@gmail.com>
2020-11-29 21:36:59 +0600

    package created

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+setup(name='asyncBeautifulSoup',
+version='1.0',
+description='Testing installation of Package',
+url='#',
+author='Muntakimur rahaman',
+author_email='muntakim1104001@gmail.com',
+license='MIT',
+packages=['asyncBeautifulSoup'],
+zip_safe=False)
